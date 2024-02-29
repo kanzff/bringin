72bea9595e449b081fb47a045876375b15752179
daus <rivakanzf@gmail.com>
2024-02-29 12:36:22 +0700

    checkout page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CheckoutCard from './CheckoutCard'
+import { Button } from 'flowbite-react'
 
 
 const Cart = ({cart, addToCart}) => {
@@ -10,14 +11,28 @@ const Cart = ({cart, addToCart}) => {
         <h1>Your cart is empty</h1>
       }
       {!!cart.length &&
-        <div className='flex flex-col gap-4'>
-          {
-            cart.map((c, i) => {
-              return (
-                <CheckoutCard product={c} addToCart={addToCart} key={i}/>
-              )
-            })
-          }
+        <div className='flex justify-between'>
+          <div className='flex flex-col gap-4'>
+            {
+              cart.map((c, i) => {
+                return (
+                  <CheckoutCard product={c} addToCart={addToCart} key={i}/>
+                )
+              })
+            }
+          </div>
+          <div className='w-1/4'>
+            <div className='ml-12  py-8 flex justify-center bg-white border border-gray-200 rounded-md shadow dark:bg-gray-800 dark:border-gray-700'>
+              <div className='w-72'>
+                <div className='flex justify-between mb-4'>
+                  <h1 className='font-bold'>Total Price</h1>
+                  <h1 className='font-bold'>$ 1240</h1>
+                </div>
+                <hr class="my-4 h-0.5 bg-slate-950"></hr>
+                <Button className='w-full'>Buy</Button>
+              </div>
+            </div>
+          </div>
         </div>
       }
     </div>
